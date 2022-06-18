af11c1b0a9f1193a1a95039bd44c7de9d3af6c09
SAUL LOPEZ <ing.xicotencatl.lopez@gmail.com>
2022-06-17 23:01:33 -0500

    Script get a litte more functionality. It runs inside a vitual environment made by pipvenv. Activation command line is 'pipenv shell'; what happens if I need to create another virtual environment. How can I distinct them?

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask import jsonify
+# from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def dummy_api():
-    return("Hello moto... desde flask calnal")
+    return jsonify(text_message = "Just print this in the local host (500)")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
